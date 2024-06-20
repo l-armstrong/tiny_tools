38a55304aad7526dc184170bed860330f8f28288
l-armstrong <lamone.armstrong@gmail.com>
2024-06-20 17:44:26 -0400

    add support for running a program from my shell

--- shell/main.py
+++ shell/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 def echo(*args):
@@ -6,20 +7,29 @@ def echo(*args):
         else: sys.stdout.write(x + '\n')
     sys.stdout.flush()
 
+def search_command(command, args):
+    import subprocess 
+    for f in os.environ['PATH'].split(":"):
+        command_path = f'{f}/{command}'
+        if os.path.isdir(f) and os.path.isfile(command_path):
+            subprocess.run([command_path, *args])
+            break
+    else:
+        print(f'{command}: not found')
+
 def commandtype(*args):
-    import os
     for command in args:
-        if command in commands:
+        if command in builtin_commands:
             print(f'{command} is a shell builtin')
         else:
-            for f in os.environ['PATH'].split(":"):
+            for f in os.environ["PATH"].split(":"):
                 if os.path.isdir(f) and command in os.listdir(f):
-                    print(f'{f}/{command}')
+                    print(f"{f}/{command}")
                     break
             else:
-                print(f'{command}: not found')
-            
-commands = {
+                print(f"{command}: not found")
+        
+builtin_commands = {
     "exit": lambda x: exit(int(x)), 
     "echo": echo, 
     "type": commandtype
@@ -30,10 +40,10 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command, *args = input().split()
-        if command not in commands:
-            print(f'{command}: command not found')
+        if command not in builtin_commands:
+            search_command(command, args)
         else:
-            commands[command](*args)
+            builtin_commands[command](*args)
 
 if __name__ == "__main__":
     main()
