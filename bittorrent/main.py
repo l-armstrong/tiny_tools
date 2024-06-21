fd4f03f1ada6b40b884fc7cb08eb412e047a1e66
l-armstrong <lamone.armstrong@gmail.com>
2024-06-21 19:01:59 -0400

    handle case for bencoded list

--- bittorrent/main.py
+++ bittorrent/main.py
@@ -1,49 +1,41 @@
 import json
 import sys
 
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
-
-# Examples:
-#
-# - decode_bencode(b"5:hello") -> b"hello"
-# - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
+    def decode(data):
+        if data[0:1].isdigit():
+            length, rest_data = data.split(b":", 1)
+            length = int(length)
+            return rest_data[:length], rest_data[length:]
+        elif data.startswith(b'i'):
+            delimiter = data.find(b'e')
+            if delimiter == -1: raise ValueError("Invalid encoded int value")
+            return int(data[1:delimiter]), data[delimiter+1:]
+        elif data.startswith(b'l'):
+            res = []
+            data = data[1:]
+            while not data.startswith(b'e'):
+                value, data = decode(data)
+                res.append(value)
+            return res, data[1:]
+        else:
             raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
-    elif chr(bencoded_value[0]) == 'i':
-        if chr(bencoded_value[-1]) != 'e':
-            raise ValueError("Invalud encoded value")
-        return bencoded_value[1:-1]
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+    decoded_value, _ = decode(bencoded_value)
+    return decoded_value
 
 
 def main():
     command = sys.argv[1]
-
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
         def bytes_to_str(data):
             if isinstance(data, bytes):
-                if data.isdigit() or chr(data[0]) == '-' and chr(data[1]).isdigit(): return int(data)
                 return data.decode()
 
             raise TypeError(f"Type not serializable: {type(data)}")
-
-        # Uncomment this block to pass the first stage
-        #print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
-
-
+    
 if __name__ == "__main__":
     main()
