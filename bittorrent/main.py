5c20c7f9696c02604aa2c3e217d140dc21bc45dc
l-armstrong <lamone.armstrong@gmail.com>
2024-06-21 23:17:46 -0400

    add parse torrent file

--- bittorrent/main.py
+++ bittorrent/main.py
@@ -28,14 +28,11 @@ def decode_bencode(bencoded_value):
             return res, data[1:]
         else:
             raise ValueError("Invalid encoded value")
-    
     decoded_value, _ = decode(bencoded_value)
     return decoded_value
 
-
 def main():
     command = sys.argv[1]
-
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
 
@@ -43,7 +40,13 @@ def main():
             if isinstance(data, bytes):
                 return data.decode()
             raise TypeError(f"Type not serializable: {type(data)}")
+        
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+    elif command == "info":
+        with open(sys.argv[2], 'rb') as f:
+            bencoded_file = f.read()
+        decoded_file = decode_bencode(bencoded_file)
+        print(f"Tracker URL: {decoded_file['announce'].decode()}\nLength: {decoded_file['info']['length']}")
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
