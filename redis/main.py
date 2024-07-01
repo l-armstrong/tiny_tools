6083e9bcfa3c7333bb95b5cd44cedcc2bf5fa775
l-armstrong <lamone.armstrong@gmail.com>
2024-07-01 19:14:54 -0400

    add support for the set and get methods

--- redis/main.py
+++ redis/main.py
@@ -13,7 +13,12 @@ import threading
 #         arg = data.split(b'\r\n')[4]
 #         return f'${len(arg)}\r\n{arg.decode()}\r\n'.encode()
 
+store = {}
+
 def parse_resp(data):
+    """ Redis serialization protocol parser
+        https://redis.io/docs/latest/develop/reference/protocol-spec/
+    """
     client_request = [
         element 
         for element in data.decode().split()
@@ -26,13 +31,19 @@ def parse_resp(data):
         case "echo":
             arg = client_request[1]
             return f'${len(arg)}\r\n{arg}\r\n'.encode()
+        case "set":
+            key, value = client_request[1], client_request[2]
+            store[key] = value
+            return "+OK\r\n".encode()
+        case "get":
+            if (key := client_request[1]) in store:
+                return f'${len(store[key])}\r\n{store[key]}\r\n'.encode()
+            return "$-1\r\n".encode() # Key doesn't exist. Return null bulk string
 
 def process_client(connection, address):
-    data = connection.recv(1024)
-    while data:
+    while data := connection.recv(1024):
         response = parse_resp(data)
         connection.sendall(response)
-        data = connection.recv(1024)
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
